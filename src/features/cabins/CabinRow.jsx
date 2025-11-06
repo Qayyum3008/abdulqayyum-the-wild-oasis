03e3588d68b8b07330beda47cb5d0bbd0454e1c3
unknown <oyedepoabdulqayyum@gmail.com>
2025-11-06 06:39:55 +0100

    Fixing some error bugs

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -12,7 +12,7 @@ import Menus from "../../ui/Menus";
 const Img = styled.img`
   display: block;
   width: 6.4rem;
-  aspect-ratio: 3 / 2;
+  aspect-ratio: 1;
   object-fit: cover;
   object-position: center;
   transform: scale(1.5) translateX(-7px);
